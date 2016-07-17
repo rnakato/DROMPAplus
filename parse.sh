108998f7e3759a344baff28bb37afa4630309ef3
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-07-18 00:23:39 +0900

    parse

---
+++ parse.sh
@@ -0,0 +1,6 @@
+/home/rnakato/git/DROMPAplus/parsejaccard.pl 
+for file in `ls parse2wigdir/*-hg38.jaccard.csv | sed -e 's/parse2wigdir\///g' -e 's/.jaccard.csv//g'`
+do
+    echo -en "$file\t"
+    /home/rnakato/git/DROMPAplus/parsejaccard.pl parse2wigdir/$file.jaccard.csv
+done
--- parsejaccard.pl
+++ parsejaccard.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/perl -w
 
+if($#ARGV==-1) {
+    print "\tNSC\tRLSC\tfragment length\tBackground enrichment\tBackground uniformity\n";
+    exit;
+}
+
 open(File, $ARGV[0]) ||die "error: can't open $ARGV[0].\n";
+
+
 while(<File>){
     next if($_ eq "\n");
     last if($_ =~ /Strand shift/);
