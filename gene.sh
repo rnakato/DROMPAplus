5a8388169288b720c0b4e8f67b87596d23d46e6d
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-07-22 15:31:13 +0900

    gene

--- gene.sh
+++ gene.sh
@@ -1,10 +1,11 @@
-gene=/home/Database/Ensembl/homo_sapiens/*refFlat
+gene=/home/Database/Ensembl/homo_sapiens/Homo_sapiens.GRCh38.83.chr.refFlat
+gt=/home/Database/Ensembl/homo_sapiens/genome_table
 
-for prefix in
+for prefix in drompa/2016_010A_D_KT55-n2-m1-hg38-raw-mpbl-sm0
 do
-    ./compare_bed2tss -g $gene --refFlat -b $prefix.bed -m 2 > $prefix.comp.bed
+    /share/git/DROMPAplus/compare_bed2tss -g $gene --refFlat -b $prefix.bed -m 2 --gt $gt > $prefix.comp.bed
     s="-b $prefix.bed"
-    ./peak_occurance -g $gene -r -b $prefix.bed --genelist genelist.txt > $prefix.prmt.bed
+    /share/git/DROMPAplus/peak_occurance -g $gene -r -b $prefix.bed --genelist Matrix.KD7.genes.count.homo_sapiens.edgeR.DEGs.csv > $prefix.prmt.bed
 done
 
-./multibed2gene -g $gene -r $s
+#./multibed2gene -g $gene -r $s
