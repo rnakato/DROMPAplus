148bb54400500e43c67f13126693e9345e864591
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-07-26 16:26:50 +0900

    parse.sh

--- parse.sh
+++ parse.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 /home/rnakato/git/DROMPAplus/parsejaccard.pl 
-for file in `ls parse2wigdir/*-hg38.jaccard.csv | sed -e 's/parse2wigdir\///g' -e 's/.jaccard.csv//g'`
+for file in `ls parse2wigdir/*.jaccard.csv | sed -e 's/parse2wigdir\///g' -e 's/.jaccard.csv//g'`
 do
     echo -en "$file\t"
     /home/rnakato/git/DROMPAplus/parsejaccard.pl parse2wigdir/$file.jaccard.csv
