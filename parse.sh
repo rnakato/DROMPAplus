cfb0f5c052a5aaf7d65366bb805122956e8e3fbe
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-08-05 18:36:19 +0900

    acccu

--- parse.sh
+++ parse.sh
@@ -7,5 +7,10 @@ do
     echo -en "$file\t"
     echo -en "`grep "Fragment score" $dir/$file.fvp.csv |cut -f2`\t"
     echo -en "`grep "Read score" $dir/$file.fvp.csv |cut -f2`\t"
+    echo -en "`cat $dir/$file.mpfv.csv | awk '{m+=$2} END{print m;}'`\t"
+    echo -en "`cat $dir/$file.mpfv.csv | awk '{m+=$3} END{print m;}'`\t"
+    echo -en "`cat $dir/$file.mpfv.csv | awk '{m+=$4} END{print m;}'`\t"
+    echo -en "`cat $dir/$file.mpfv.csv | awk '{m+=$5} END{print m;}'`\t"
+    echo -en "`cat $dir/$file.mpfv.csv | awk '{m+=$6} END{print m;}'`\t"
     /home/rnakato/git/DROMPAplus/parsejaccard.pl $dir/$file.jaccard.csv
 done
