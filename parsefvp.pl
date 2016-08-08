5925c35578f304465836433ff33379cd719bb497
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-08-08 16:20:10 +0900

    insufficient

---
+++ parsefvp.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+if($#ARGV==-1) {
+    print "\tFragment score\tRead score\tlen50\tlen100\tlen150\tlen500\tlen1000\tlen2000\tlen3000\tlen10000\tlen100000\tlen1000000";
+    exit;
+}
+
+open(File, $ARGV[0]) ||die "error: can't open $ARGV[0].\n";
+
+my $n=0;
+while(<File>){
+    next if($_ eq "\n");
+    chomp;
+    
+    my @clm= split(/\t/, $_);
+    $n++;   
+
+    next if($n==3);
+    last if($n>13);
+    if($clm[0] =~ /insufficient/) {
+	print "($clm[1])\t";
+    } else {
+	print "$clm[1]\t";
+    }
+}
+close (File);
