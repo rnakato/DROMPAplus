3f3d229113269e24390d1ae5675408c313c8343b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-08-08 16:24:16 +0900

    tesg

--- pw_readmapfile.cpp
+++ pw_readmapfile.cpp
@@ -382,30 +382,27 @@ void estimateFragLength(const MyOpt::Variables &values, Mapfile &p)
 {
   if(!values.count("pair") && !values.count("nomodel")) {
     clock_t t1 = clock();
-    //    strShiftProfile(values, p, "exjaccard");
+    strShiftProfile(values, p, "exjaccard");
     clock_t t2 = clock();
     std::cout << "Jaccard Vec: " << static_cast<double>(t2 - t1) / CLOCKS_PER_SEC << "sec.\n";
 
-    //    strShiftProfile(values, p, "jaccard"); 
+    strShiftProfile(values, p, "jaccard"); 
     clock_t t3 = clock();
     std::cout << "Jaccard Bit: " << static_cast<double>(t3 - t2) / CLOCKS_PER_SEC << "sec.\n";
 
     strShiftProfile(values, p, "fvp"); 
     clock_t t4 = clock();
     std::cout << "Fragment variability: " << static_cast<double>(t4 - t3) / CLOCKS_PER_SEC << "sec.\n";
-   
-    
-    //strShiftProfile(values, p, "hdp");
+
+    strShiftProfile(values, p, "hdp");
     clock_t t5 = clock();
     std::cout << "Hamming: " << static_cast<double>(t5 - t4) / CLOCKS_PER_SEC << "sec.\n";
 
-    //    strShiftProfile(values, p, "ccp");
+    strShiftProfile(values, p, "ccp");
     clock_t t6 = clock();
     std::cout << "ccp: " << static_cast<double>(t6 - t5) / CLOCKS_PER_SEC << "sec.\n";
   }
-  
-  exit(0);
-  
+
   p.genome.calcdepthAll(p.getflen(values));
   p.genome.setF5All(p.getflen(values));
 }
