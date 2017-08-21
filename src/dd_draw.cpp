7994a6e651514fa9b61c8b56d9e2bcae05de4ecb
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-08-21 18:25:19 +0900

    modify DROMPAplus

--- src/dd_draw.cpp
+++ src/dd_draw.cpp
@@ -8,217 +8,6 @@
 #include "dd_draw_p.hpp"
 #include "SSP/common/inline.hpp"
 
-class ChIPDataFrame : public DataFrame {
- public:
-  ChIPDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, pair.label, p.scale.scale_tag, refparam, wdf, hdf)
-  {}
-
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-    double value((arrays.at(pair.argvChIP)).array[i] / scale);
-    if (!value) return;
-
-    int32_t len(getbinlen(value));
-
-    cr->set_source_rgba(CLR_GREEN3, 1);
-    if (!viz) {
-      rel_yline(cr, xcen, yaxis, len);
-    } else {
-      if(len <=-1 && value <= par.barnum) rel_yline(cr, xcen, yaxis + len, 1);
-      cr->stroke();
-      cr->set_source_rgba(CLR_GREEN3, 0.5);
-      rel_yline(cr, xcen, yaxis, len);     
-      cr->stroke();
-    }
-    return;
-  }
-};
-
-void DataFrame::stroke_bindata(const DROMPA::Global &p, const SamplePairChr &pair,
-		      const std::unordered_map<std::string, ChrArray> &arrays, const int32_t nlayer)
-  {
-    int32_t binsize(pair.getbinsize());
-    int32_t sbin(par.xstart/binsize);
-    int32_t ebin(par.xend/binsize);
-    double dot_per_bin(binsize * par.dot_per_bp);
-    int32_t yaxis(par.yaxis_now);  // intに変換
-
-    int32_t thin(std::min(par.width_per_line/(1000*binsize), 20));
-
-    // peak region 色を変えるのではなく、その領域のバックを光らせる
-    double xcen(bp2axis(binsize/2));  // initial position
-    if(thin > 1) cr->set_line_width(dot_per_bin*thin);
-    else cr->set_line_width(dot_per_bin);
-
-    for (int32_t i=sbin; i<ebin; ++i, xcen += dot_per_bin) {
-      if (thin > 1 && i%thin) continue;
-      stroke_bin(pair, arrays, i, xcen, yaxis, p.drawparam.viz);
-    }
-    cr->stroke();
-    return;
-  }
-
-
-class InputDataFrame : public DataFrame {
- public:
-  InputDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, "Input", p.scale.scale_tag, refparam, wdf, hdf)
-  {}
-
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-    double value((arrays.at(pair.argvInput)).array[i] / scale);
-    if (!value) return;
-
-    int32_t len(getbinlen(value));
-
-    cr->set_source_rgba(CLR_BLUE, 1);
-    rel_yline(cr, xcen, yaxis, len);
-    return;
-  }
-};
-
-class RatioDataFrame : public DataFrame {
- public:
-  RatioDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
-  {}
-
-  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
-    if(p.drawparam.showctag) return "IP/Input";
-    else return pair.label;
-  }
-
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-    double value(CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1) / scale);  // TOTAL READ 正規化入れる
-    if (!value) return;
- 
-    int32_t len(getbinlen(value));
-
-    cr->set_source_rgba(CLR_ORANGE, 1);
-    rel_yline(cr, xcen, yaxis, len);
-    return;
-  }
-};
-
-class LogRatioDataFrame : public DataFrame { // log10(ratio)
-  int32_t barnum_minus;
-  int32_t barnum_plus;
- public:
-  LogRatioDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf),
-    barnum_minus(refparam.barnum/2), barnum_plus(refparam.barnum - barnum_minus)
-  {}
-
-  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
-    if(p.drawparam.showctag) return "IP/Input";
-    else return pair.label;
-  }
-
-  void stroke_ymem(const int32_t nlayer)
-  {
-    cr->set_source_rgba(CLR_BLACK, 1);
-
-    int32_t barnum_minus = par.barnum/2;
-    double x(0);
-    if (!nlayer) x = OFFSET_X + width_df + 7; else x = OFFSET_X - 20;
-    for(int32_t i=1; i<=par.barnum; ++i) {
-      std::string str;
-      if (i < barnum_minus) str = "1/" + std::to_string(static_cast<int>(pow(2, (barnum_minus-i) * scale)));
-      else str = std::to_string(static_cast<int>(pow(2, (i-barnum_minus) * scale)));
-      
-      showtext_cr(cr, x, par.yaxis_now - i*(par.ystep - 1.5), str, 9);
-    }
-    return;
-  }
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-
-    double data(CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1));  // TOTAL READ 正規化入れる
-    if (!data) return;
-
-    double value(log10(data)/scale);
-    int32_t len(0);
-    
-    if(value>0) len = -std::min(par.ystep*value, height_df/2);
-    else        len = -std::max(par.ystep*value, -height_df/2);
-    
-    cr->set_source_rgba(CLR_ORANGE, 1);
-    rel_yline(cr, xcen, yaxis-height_df/2, len);
-    return;
-  }
-};
-
-class PinterDataFrame : public DataFrame {
- public:
-  PinterDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
-  {}
-
-  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
-    if(p.drawparam.showctag || p.drawparam.showratio) return "pval (ChIP internal)";
-    else return pair.label;
-  }
-
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-    // double data = zero_inflated_binomial_test(WIGARRAY2VALUE(sample->ChIP->data[i]), sample->ChIP->nb_p, sample->ChIP->nb_n);
-    double data = CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1);
-    double value(data/scale);
-    if (!value) return;
- 
-    int32_t len(getbinlen(value));
-
-    //    if(data > p->pthre_internal) cr->set_source_rgba(CLR_PINK, 1);
-    //else cr->set_source_rgba(CLR_CLR_GRAY, 1);
-    cr->set_source_rgba(CLR_PINK, 1);
-    rel_yline(cr, xcen, yaxis, len);
-    return;
-  }
-};
-
-class PenrichDataFrame : public DataFrame {
- public:
-  PenrichDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
-		   const DParam &refparam, const double wdf, const double hdf):
-    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
-  {}
-
-  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
-    if(p.drawparam.showctag || p.drawparam.showratio) return "pval (IP/Input)";
-    else return pair.label;
-  }
-
-  void stroke_bin(const SamplePairChr &pair,
-		  const std::unordered_map<std::string, ChrArray> &arrays,
-		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz) {
-    //double data = binomial_test(WIGARRAY2VALUE(sample->ChIP->data[i]), WIGARRAY2VALUE(sample->Input->data[i]), sample->comp->genome->ratio);
-    double data = CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1);
-    double value(data/scale);
-    if (!value) return;
- 
-    int32_t len(getbinlen(value));
-
-    //    if(data > p->pthre_enrich) cr->set_source_rgba(CLR_PINK, 1);
-    //else cr->set_source_rgba(CLR_CLR_GRAY, 1);
-    cr->set_source_rgba(CLR_PINK, 1);
-    rel_yline(cr, xcen, yaxis, len);
-    return;
-  }
-};
-
       /*    if (p->gapfile && d->gaparray[i] >= GAP_THRE){
 	    cr->set_source_rgba(CLR_BLACK, 0.3);
 	    rel_yline(cr, xcen, yaxis - height_df, height_df);
@@ -229,14 +18,161 @@ class PenrichDataFrame : public DataFrame {
 	    cr->stroke();
 	    }*/
 
+
+void ChIPDataFrame::stroke_bin(const SamplePairChr &pair,
+			       const std::unordered_map<std::string, ChrArray> &arrays,
+			       const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  double value((arrays.at(pair.argvChIP)).array[i] / scale);
+  if (!value) return;
+
+  int32_t len(getbinlen(value));
+
+  cr->set_source_rgba(CLR_GREEN3, 1);
+  if (!viz) {
+    rel_yline(cr, xcen, yaxis, len);
+  } else {
+    if(len <=-1 && value <= par.barnum) rel_yline(cr, xcen, yaxis + len, 1);
+    cr->stroke();
+    cr->set_source_rgba(CLR_GREEN3, 0.5);
+    rel_yline(cr, xcen, yaxis, len);     
+    cr->stroke();
+  }
+  return;
+}
+
+void InputDataFrame::stroke_bin(const SamplePairChr &pair,
+				const std::unordered_map<std::string, ChrArray> &arrays,
+				const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  double value((arrays.at(pair.argvInput)).array[i] / scale);
+  if (!value) return;
+
+  int32_t len(getbinlen(value));
+
+  cr->set_source_rgba(CLR_BLUE, 1);
+  rel_yline(cr, xcen, yaxis, len);
+  return;
+}
+
+void RatioDataFrame::stroke_bin(const SamplePairChr &pair,
+				const std::unordered_map<std::string, ChrArray> &arrays,
+				const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  double value(CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1) / scale);  // TOTAL READ 正規化入れる
+  if (!value) return;
+ 
+  int32_t len(getbinlen(value));
+
+  cr->set_source_rgba(CLR_ORANGE, 1);
+  rel_yline(cr, xcen, yaxis, len);
+  return;
+}
+
+void LogRatioDataFrame::stroke_bin(const SamplePairChr &pair,
+				   const std::unordered_map<std::string, ChrArray> &arrays,
+				   const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  double data(CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1));  // TOTAL READ 正規化入れる
+  if (!data) return;
+
+  double value(log10(data)/scale);
+  int32_t len(0);
+    
+  if(value>0) len = -std::min(par.ystep*value, height_df/2);
+  else        len = -std::max(par.ystep*value, -height_df/2);
+    
+  cr->set_source_rgba(CLR_ORANGE, 1);
+  rel_yline(cr, xcen, yaxis-height_df/2, len);
+  return;
+}
+
+void PinterDataFrame::stroke_bin(const SamplePairChr &pair,
+				 const std::unordered_map<std::string, ChrArray> &arrays,
+				 const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  // double data = zero_inflated_binomial_test(WIGARRAY2VALUE(sample->ChIP->data[i]), sample->ChIP->nb_p, sample->ChIP->nb_n);
+  double data = CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1);
+  double value(data/scale);
+  if (!value) return;
+ 
+  int32_t len(getbinlen(value));
+
+  //    if(data > p->pthre_internal) cr->set_source_rgba(CLR_PINK, 1);
+  //else cr->set_source_rgba(CLR_CLR_GRAY, 1);
+  cr->set_source_rgba(CLR_PINK, 1);
+  rel_yline(cr, xcen, yaxis, len);
+  return;
+}
+
+void PenrichDataFrame::stroke_bin(const SamplePairChr &pair,
+				  const std::unordered_map<std::string, ChrArray> &arrays,
+				  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)
+{
+  //double data = binomial_test(WIGARRAY2VALUE(sample->ChIP->data[i]), WIGARRAY2VALUE(sample->Input->data[i]), sample->comp->genome->ratio);
+  double data = CALCRATIO(arrays.at(pair.argvChIP).array[i], arrays.at(pair.argvInput).array[i], 1);
+  double value(data/scale);
+  if (!value) return;
+ 
+  int32_t len(getbinlen(value));
+
+  //    if(data > p->pthre_enrich) cr->set_source_rgba(CLR_PINK, 1);
+  //else cr->set_source_rgba(CLR_CLR_GRAY, 1);
+  cr->set_source_rgba(CLR_PINK, 1);
+  rel_yline(cr, xcen, yaxis, len);
+  return;
+}
+
+
+void DataFrame::stroke_dataframe(const DROMPA::Global &p, const int32_t nlayer) {
+  stroke_frame();
+  /* y memory */
+  cr->set_line_width(0.4);
+  cr->set_source_rgba(CLR_BLACK, 0.5);
+  for(int32_t i=0; i<par.barnum; ++i) rel_xline(cr, OFFSET_X, par.yaxis_now - i*par.ystep, width_df);
+  cr->stroke();
+
+  if (p.drawparam.isshowymem()) stroke_ymem(nlayer);
+  if (!nlayer && p.drawparam.isshowylab()) {
+    cr->set_source_rgba(CLR_BLACK, 1);
+    showtext_cr(cr, 50, par.yaxis_now - height_df/2, label, 12);
+  }
+  return;
+}
+
+void DataFrame::stroke_bindata(const DROMPA::Global &p, const SamplePairChr &pair,
+			       const std::unordered_map<std::string, ChrArray> &arrays, const int32_t nlayer)
+{
+  int32_t binsize(pair.getbinsize());
+  int32_t sbin(par.xstart/binsize);
+  int32_t ebin(par.xend/binsize);
+  double dot_per_bin(binsize * par.dot_per_bp);
+  int32_t yaxis(par.yaxis_now);  // intに変換
+
+  int32_t thin(std::min(par.width_per_line/(1000*binsize), 20));
+
+  // peak region 色を変えるのではなく、その領域のバックを光らせる
+  double xcen(par.bp2xaxis(binsize/2)); // initial position
+  if(thin > 1) cr->set_line_width(dot_per_bin*thin);
+  else cr->set_line_width(dot_per_bin);
+
+  for (int32_t i=sbin; i<ebin; ++i, xcen += dot_per_bin) {
+    if (thin > 1 && i%thin) continue;
+    stroke_bin(pair, arrays, i, xcen, yaxis, p.drawparam.viz);
+  }
+  cr->stroke();
+  return;
+}
+
 void Page::stroke_each_layer(const DROMPA::Global &p, const SamplePairChr &pair, const int32_t nlayer)
 {
-  //  if(d->visualize_p_inter)  stroke_readdist(p, d, cr, sample, xstart, xend, LTYPE_PVALUE_INTER,  nlayer);
-  //if(d->visualize_p_enrich) stroke_readdist(p, d, cr, sample, xstart, xend, LTYPE_PVALUE_ENRICH, nlayer);
+  
+  if (p.drawparam.showpinter) stroke_readdist<PinterDataFrame>(p, pair, nlayer);
+  if (p.drawparam.showpenrich && pair.argvInput!="") stroke_readdist<PenrichDataFrame>(p, pair, nlayer);
   if (p.drawparam.showratio && pair.argvInput!="") {
     //    if(p->ftype==FTYPE_GV)  stroke_readdist(p, d, cr, sample, xstart, xend, LTYPE_RATIO_GV, nlayer);
     //else
-    if (p.drawparam.showratio == 1)   stroke_readdist<RatioDataFrame>(p, pair, nlayer);
+    if (p.drawparam.showratio == 1) stroke_readdist<RatioDataFrame>(p, pair, nlayer);
     else if (p.drawparam.showratio == 2) stroke_readdist<LogRatioDataFrame>(p, pair, nlayer);
   }
   if (p.drawparam.showctag) stroke_readdist<ChIPDataFrame>(p, pair, nlayer);
@@ -245,7 +181,7 @@ void Page::stroke_each_layer(const DROMPA::Global &p, const SamplePairChr &pair,
 }
 
 
-void Page::draw(const DROMPA::Global &p, const int32_t page_curr)
+void Page::draw(const DROMPA::Global &p, const int32_t page_curr, const std::string &chrname, const int32_t region_no)
 {
   int32_t line_start, line_end;
   std::tie(line_start, line_end) = get_start_end_linenum(page_curr, p.drawparam.getlpp());
@@ -271,10 +207,10 @@ void Page::draw(const DROMPA::Global &p, const int32_t page_curr)
     if(d->internum){
       for(j=0; j<d->internum; ++j) draw_interaction(cr, &(d->inter[j]), xstart, xend, chr);
       }*/
-    double ytemp = par.yaxis_now;
+    //    double ytemp = par.yaxis_now;
     int32_t nlayer = 0;
     for(size_t j=0; j<pairs.size(); ++j) stroke_each_layer(p, pairs[j], nlayer);
-    //stroke_xaxis_num(d, cr, xstart, xend, yaxis_now, 9);
+    stroke_xaxis_num(9);
 
     //    if(d->bednum) draw_bedfile(d, cr, xstart, xend, chr);
     //if(d->repeat.argv) draw_repeat(d, cr, xstart, xend);
@@ -288,7 +224,14 @@ void Page::draw(const DROMPA::Global &p, const int32_t page_curr)
       
     par.yaxis_now += MERGIN_BETWEEN_LINE;
   }
-  //  print_pagetitle(cr, g->chr[chr].name, page_curr, num_page, region_no);
+
+  // page titile
+  std::string title;
+  if(par.num_page>1) title = chrname + "_" + std::to_string(region_no+1) + "_" + std::to_string(page_curr+1);
+  else               title = chrname + "_" + std::to_string(region_no+1);
+  cr->set_source_rgba(CLR_BLACK, 1);
+  showtext_cr(cr, 50, 30, title, 16);
+  
   cr->show_page();
   return;
 }
@@ -311,7 +254,7 @@ void Figure::DrawData(DROMPA::Global &p)
   for(int32_t i=0; i<num_page; ++i) {
     std::cout << boost::format("   page %5d/%5d/%5d\r") % (i+1) % num_page % (region_no+1) << std::flush;
     Page page(p, arrays, pairs, surface, start, end);
-    page.draw(p, i);
+    page.draw(p, i, "chr" + chrname, region_no);
   }
 #else
   std::cout << "You must compile cairo with PDF support for DROMPA." << std::endl;
@@ -326,7 +269,7 @@ int32_t DROMPA::DrawParam::getHeightEachSample(const SamplePairChr &pair) const 
   if (showctag)                            { height += lineheight; ++n; }
   if (showitag==1 && pair.argvInput != "") { height += lineheight; ++n; }
   if (showratio   && pair.argvInput != "") { height += lineheight; ++n; }
-  if (showpinter  && pair.argvInput != "") { height += lineheight; ++n; }
+  if (showpinter)                          { height += lineheight; ++n; }
   if (showpenrich && pair.argvInput != "") { height += lineheight; ++n; }
   height += MERGIN_BETWEEN_DATA * (n-1);
   
--- src/dd_draw_p.hpp
+++ src/dd_draw_p.hpp
@@ -31,6 +31,13 @@ namespace {
   int32_t boxheight_graph(80);
 }
 
+inline int32_t setline(int32_t start, int32_t interval)
+{
+  int32_t posi(start-1);
+  if(!posi%interval) return posi;
+  else return (posi/interval +1) * interval;
+}
+
 inline void showtext_cr(Cairo::RefPtr<Cairo::Context> cr, const double x, const double y, const std::string &str, const int32_t fontsize)
 {
   cr->move_to(x, y);
@@ -81,6 +88,8 @@ public:
     if(i==num_line-1) xend = end;
     else xend = start + (i+1) * width_per_line -1;
   }
+  
+  double bp2xaxis(const int32_t bp) const { return bp * dot_per_bp + OFFSET_X; }
 };
 
 class Page {
@@ -116,19 +125,84 @@ class Page {
     T df(cr, p, pair, par, getWidthDf(), getHeightDf());
     df.stroke_bindata(p, pair, arrays, nlayer);
     df.stroke_dataframe(p, nlayer);
-    // if(!nlayer) stroke_xaxis(d, cr, xstart, xend);*/
+    if(!nlayer) stroke_xaxis();
     return;
   }
 
-  
-  //  void stroke_dataframe(const DROMPA::Global &p, const SamplePairChr &pair, const LineType type, const int32_t nlayer);
-  //void stroke_bindata(const DROMPA::Global &p, const SamplePairChr &pair, const LineType type, const int32_t nlayer);
-  void draw(const DROMPA::Global &p, const int32_t page_curr);
+  void draw(const DROMPA::Global &p, const int32_t page_curr, const std::string &chrname, const int32_t region_no);
 
   void set_xstart_xend(const int32_t i) {
     par.set_xstart_xend(i);
   }
 
+  /*  
+  int32_t set_interval_large() {
+    int32_t interval;
+    if(d->gw==true){
+      if(d->large_genome==false) interval = 100*NUM_1K;  // 100kbp
+      else interval = 10*NUM_1M;  // 10Mbp
+    }else{
+      interval = width_per_line/10;
+    }
+    return interval;
+    }*/
+
+  void stroke_xaxis_num(const int32_t fontsize) {
+    int32_t mega, kilo;
+    double x;
+    int32_t interval(par.width_per_line/10);
+    
+    cr->set_source_rgba(CLR_BLACK, 1);
+    for(int32_t i=setline(par.xstart, interval); i<=par.xend; i+=interval) {
+      std::string str;
+      x = par.bp2xaxis(i - par.xstart);
+      /*  if(d->gw==true){
+	if(d->large_genome==false) sprintf(str, "%dk", i/NUM_1K);
+	else sprintf(str, "%dM", i/NUM_1M);
+	showtext_cr(cr, x - 3*strlen(str), par.yaxis_now +10, str, fontsize);
+	}else{*/
+	mega = i/NUM_1M;
+	kilo = (i%NUM_1M)/NUM_1K;
+	if (par.width_per_line > 10*NUM_1K) str = float2string(i/static_cast<double>(NUM_1M), 3) + "M"; 
+	else if (par.width_per_line > 10) {
+	  if (mega) str = std::to_string(mega) + "," + float2string((i%NUM_1M)/static_cast<double>(NUM_1K), 3) + "K";
+	  else str = float2string((i%NUM_1M)/static_cast<double>(NUM_1K), 3) + "K";
+	} else {
+	  if (mega) str = std::to_string(mega) + "," + std::to_string(kilo) + "," + std::to_string(i%NUM_1K);
+	  else if (kilo) str = std::to_string(kilo) + "," + std::to_string(i%NUM_1K);
+	  else str = std::to_string(i%NUM_1K);
+	}
+	showtext_cr(cr, x - 3*str.length(), par.yaxis_now +10, str, fontsize);
+	//      }
+    }
+    return;
+  }
+
+  
+void stroke_xaxis(){
+  double x;
+  //  int32_t interval_large = set_interval_large(d);
+  int32_t interval_large(par.width_per_line/10);
+  int32_t interval(interval_large/10);
+
+  cr->set_source_rgba(CLR_BLACK, 1);
+  for(int32_t i=setline(par.xstart, interval); i<=par.xend; i+=interval) {
+    x = par.bp2xaxis(i-par.xstart);
+    
+    if (!(i%interval_large)) {
+      cr->set_line_width(1);
+      rel_yline(cr, x, par.yaxis_now-4, 8);
+    } else {
+      cr->set_line_width(0.5);
+      rel_yline(cr, x, par.yaxis_now-1.5, 3);
+    }
+    cr->stroke();
+  }
+  return;
+}
+
+  
+  
   std::tuple<int32_t, int32_t> get_start_end_linenum(const int32_t page, const int32_t linenum_per_page) const {
     int32_t start(0), end(0);
     start = page * linenum_per_page;
@@ -178,31 +252,129 @@ protected:
     }
     return;
   }
-  void stroke_dataframe(const DROMPA::Global &p, const int32_t nlayer) {
-    stroke_frame();
-    /* y memory */
-    cr->set_line_width(0.4);
-    cr->set_source_rgba(CLR_BLACK, 0.5);
-    for(int32_t i=0; i<par.barnum; ++i) rel_xline(cr, OFFSET_X, par.yaxis_now - i*par.ystep, width_df);
-    cr->stroke();
-
-    if (p.drawparam.isshowymem()) stroke_ymem(nlayer);
-    if (!nlayer && p.drawparam.isshowylab()) {
-      cr->set_source_rgba(CLR_BLACK, 1);
-      showtext_cr(cr, 50, par.yaxis_now - height_df/2, label, 12);
-    }
-    return;
-  }
-  
+  void stroke_dataframe(const DROMPA::Global &p, const int32_t nlayer);
   void stroke_bindata(const DROMPA::Global &p, const SamplePairChr &pair,
 		      const std::unordered_map<std::string, ChrArray> &arrays, const int32_t nlayer);
 
-  double bp2axis(const double v) const { return v * par.dot_per_bp + OFFSET_X; }
   int32_t getbinlen(const double value) const { return -std::min(par.ystep*value, height_df); }
   
   virtual void stroke_bin(const SamplePairChr &pair,
 			  const std::unordered_map<std::string, ChrArray> &arrays,
 			  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz)=0;
 };
+
+
+class ChIPDataFrame : public DataFrame {
+ public:
+  ChIPDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, pair.label, p.scale.scale_tag, refparam, wdf, hdf)
+  {}
+
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
+
+class InputDataFrame : public DataFrame {
+ public:
+  InputDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, "Input", p.scale.scale_tag, refparam, wdf, hdf)
+  {}
+
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
+class RatioDataFrame : public DataFrame {
+ public:
+  RatioDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
+  {}
+
+  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
+    if(p.drawparam.showctag) return "IP/Input";
+    else return pair.label;
+  }
+
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
+class LogRatioDataFrame : public DataFrame { // log10(ratio)
+  int32_t barnum_minus;
+  int32_t barnum_plus;
+ public:
+  LogRatioDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf),
+    barnum_minus(refparam.barnum/2), barnum_plus(refparam.barnum - barnum_minus)
+  {}
+
+  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
+    if(p.drawparam.showctag) return "IP/Input";
+    else return pair.label;
+  }
+
+  void stroke_ymem(const int32_t nlayer)
+  {
+    cr->set_source_rgba(CLR_BLACK, 1);
+
+    int32_t barnum_minus = par.barnum/2;
+    double x(0);
+    if (!nlayer) x = OFFSET_X + width_df + 7; else x = OFFSET_X - 20;
+    for(int32_t i=1; i<=par.barnum; ++i) {
+      std::string str;
+      if (i < barnum_minus) str = "1/" + std::to_string(static_cast<int>(pow(2, (barnum_minus-i) * scale)));
+      else str = std::to_string(static_cast<int>(pow(2, (i-barnum_minus) * scale)));
+      
+      showtext_cr(cr, x, par.yaxis_now - i*(par.ystep - 1.5), str, 9);
+    }
+    return;
+  }
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
+class PinterDataFrame : public DataFrame {
+ public:
+  PinterDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
+  {}
+
+  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
+    if(p.drawparam.showctag || p.drawparam.showratio) return "pval (ChIP internal)";
+    else return pair.label;
+  }
+
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
+class PenrichDataFrame : public DataFrame {
+ public:
+  PenrichDataFrame(const Cairo::RefPtr<Cairo::Context> cr_, const DROMPA::Global &p, const SamplePairChr &pair,
+		   const DParam &refparam, const double wdf, const double hdf):
+    DataFrame(cr_, getlabel(p, pair), p.scale.scale_ratio, refparam, wdf, hdf)
+  {}
+
+  const std::string getlabel(const DROMPA::Global &p, const SamplePairChr &pair) const {
+    if(p.drawparam.showctag || p.drawparam.showratio) return "pval (IP/Input)";
+    else return pair.label;
+  }
+
+  void stroke_bin(const SamplePairChr &pair,
+		  const std::unordered_map<std::string, ChrArray> &arrays,
+		  const int32_t i, const double xcen, const int32_t yaxis, const int32_t viz);
+};
+
 
 #endif /* _DD_READFILE_P_H_ */
