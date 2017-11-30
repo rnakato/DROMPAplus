edc0ebe0fc36488696ab8df0021aa894fe9ca926
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-11-30 15:33:44 +0900

    rm SSP

--- src/SSP/common/alglib/README.md
+++
@@ -1,3 +0,0 @@
-# alglib
-
-The alglib-3.10.0 was obtained from [ALGLIB project](http://www.alglib.net/)
--- src/SSP/src/BpStatus.hpp
+++
@@ -1,9 +0,0 @@
-/* Copyright(c)  Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
- * All rights reserved.
- */
-#ifndef _BPSTATUS_HPP_
-#define _BPSTATUS_HPP_
-
-enum class BpStatus: uint8_t {UNMAPPABLE, INBED, MAPPABLE, COVREAD_ALL, COVREAD_NORM};
-
-#endif /* _BPSTATUS_HPP_ */
