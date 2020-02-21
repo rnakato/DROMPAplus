861f3f1e51310b8f0996ce2e478320570ecfc663
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-02-22 08:40:50 +0900

    version 1.4.0

--- tutorial/sample.yeast.sh
+++ tutorial/sample.yeast.sh
@@ -2,16 +2,26 @@
 # DROMPAplus sample scripts
 # Replication analysis of S. cerevisiae
 
-https://drive.google.com/open?id=1f5H-umPgjzmDYLVHBdlIMWQXtt91S6Fc
-https://drive.google.com/open?id=1f991hi-V9ITAWeUzr3AkqyRY8CbFTBXO
-https://drive.google.com/open?id=1fDUgeo8IvhI-fikQv_PDCiDmNc9cBfWT
-https://drive.google.com/open?id=1fGjY5nlhjePk_0TXKC5bLOzmdrer7IHh
-https://drive.google.com/open?id=1fJh-f3CDNc7wAkuN79rndjptVq_qYU6k
-https://drive.google.com/open?id=1fNRt1uvA1CQrIfb9NSJq3hYL9-GGJnXv
+### Download cram files from the URLs below:
+# https://drive.google.com/open?id=1f5H-umPgjzmDYLVHBdlIMWQXtt91S6Fc
+# https://drive.google.com/open?id=1f991hi-V9ITAWeUzr3AkqyRY8CbFTBXO
+# https://drive.google.com/open?id=1fDUgeo8IvhI-fikQv_PDCiDmNc9cBfWT
+# https://drive.google.com/open?id=1fGjY5nlhjePk_0TXKC5bLOzmdrer7IHh
+# https://drive.google.com/open?id=1fJh-f3CDNc7wAkuN79rndjptVq_qYU6k
+# https://drive.google.com/open?id=1fNRt1uvA1CQrIfb9NSJq3hYL9-GGJnXv
+
+# parse2wig (make BigWig)
+gt=../data/genometable/genometable.scer.txt
+mptable=../data/mptable/mptable.UCSC.sacCer3.50mer.flen150.txt
+for cell in YST1019_Gal YST1019_Raf YST1053_Gal; do
+for min in 0min 60min; do
+cram=${cell}_${min}-n2-k1.sort.cram
+parse2wig+ -i $cram  -o ${cell}_${min} --gt $gt --mptable $mptable -n GR
+done
+done
 
 exit
 
-gt=../data/genometable/genometable.sacCer3.txt
 dir=parse2wigdir+
 drompa+ GV \
 	-i $dir/H3K4me3.100000.bw,$dir/Input.100000.bw,H3K4me3   \
@@ -29,24 +39,10 @@ drompa+ GV \
 	--ideogram ../data/ideogram/hg19.tsv
  exit
 
-# Get BAM files
-wget -nc http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwHistone/wgEncodeUwHistoneK562H3k4me3StdAlnRep1.bam
-wget -nc http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwHistone/wgEncodeUwHistoneK562H3k27me3StdAlngtRep1.bam
-wget -nc http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwHistone/wgEncodeUwHistoneK562H3k36me3StdAlnRep1.bam
-wget -nc http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwHistone/wgEncodeUwHistoneK562InputStdAlnRep1.bam
-
 # Get refFlat (gene annotation)
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/refFlat.txt.gz
 gunzip refFlat.txt.gz
 
-# parse2wig (make BigWig)
-### When the current directory is DROMPAplus/scripts
-gt=../data/genometable/genometable.hg19.txt
-mptable=../data/mptable/mptable.UCSC.hg19.36mer.flen150.txt
-parse2wig+ -i wgEncodeUwHistoneK562H3k4me3StdAlnRep1.bam  -o H3K4me3  --gt $gt --mptable $mptable -n GR
-parse2wig+ -i wgEncodeUwHistoneK562H3k27me3StdAlnRep1.bam -o H3K27me3 --gt $gt --mptable $mptable -n GR
-parse2wig+ -i wgEncodeUwHistoneK562H3k36me3StdAlnRep1.bam -o H3K36me3 --gt $gt --mptable $mptable -n GR
-parse2wig+ -i wgEncodeUwHistoneK562InputStdAlnRep1.bam    -o Input    --gt $gt --mptable $mptable -n GR
 
 
 # Make pdf
