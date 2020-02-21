fa7000b9c18f1cb4513fae81e8ed88f0d9c72975
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-02-21 17:58:41 +0900

    version 1.4.0

--- otherbins/drompa.heatmap.py
+++ otherbins/drompa.heatmap.py
@@ -6,8 +6,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import argparse
 import sys
+import os
 
-def drawHeatmap(input, output, vmax, sortid):
+def drawHeatmap(input, output, vmax, sortid, notsort):
     nsample = len(input)
     figwidth = 2 + 2*nsample
     fig = plt.figure(figsize=(figwidth, 16))
@@ -21,11 +22,14 @@ def drawHeatmap(input, output, vmax, sortid):
 
     for i, sample in enumerate(input):
         df = pd.read_csv(sample, sep="\t", index_col=0)
-        df["ref"] = df0
-#        print(df)
-        df_sort = df.sort_values('ref', ascending=False)
-        del df_sort['ref']
-#        print(df_sort)
+        if notsort:
+            df_sort = df
+        else:
+            df["ref"] = df0
+            #        print(df)
+            df_sort = df.sort_values('ref', ascending=False)
+            del df_sort['ref']
+            #        print(df_sort)
 
         ax = plt.subplot(1, nsample, i+1)
         sns.heatmap(df_sort,
@@ -41,6 +45,7 @@ if(__name__ == '__main__'):
     parser.add_argument("-o","--output", help="Output prefix", type=str)
     parser.add_argument("--vmax", help="Max value of heatmap", type=str, default=100)
     parser.add_argument("--sortid", help="Sample number for sorting", type=str, default=0)
+    parser.add_argument("--notsort", help="do not sort the rows", action='store_true')
 
     args = parser.parse_args()
 #    print(args)
@@ -51,4 +56,9 @@ if(__name__ == '__main__'):
     print ("Output file:")
     print ("\t" + args.output + ".png")
 
-    drawHeatmap(args.input, args.output, args.vmax, int(args.sortid))
+    for sample in args.input:
+        if not os.path.isfile(sample):
+            print ("Error: " + sample + " is not found.")
+            sys.exit()
+
+    drawHeatmap(args.input, args.output, args.vmax, int(args.sortid), args.notsort)
